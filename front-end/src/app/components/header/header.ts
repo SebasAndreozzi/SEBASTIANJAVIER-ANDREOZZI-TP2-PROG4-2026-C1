32ee7131177224cf563661c4a4325d32a42bb60c
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-06-20 13:31:31 -0300

    Actualizo estilado

--- front-end/src/app/components/header/header.ts
+++ front-end/src/app/components/header/header.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
