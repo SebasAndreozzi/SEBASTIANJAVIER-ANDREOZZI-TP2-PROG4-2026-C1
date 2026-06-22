f1867901581b011e97a5675be1299ad0dc0488c4
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-06-22 11:31:49 -0300

    Corrijo coneccion

--- back_end/eslint.config.mjs
+++ back_end/eslint.config.mjs
@@ -32,7 +32,8 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unsafe-argument": "off"
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-floating-promises": "off"
     },
   },
 );
