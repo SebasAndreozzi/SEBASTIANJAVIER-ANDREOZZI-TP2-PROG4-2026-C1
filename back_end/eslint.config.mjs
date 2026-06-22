1bf5de66bc9e76e4eabe59105bf90b190aef2f5b
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-06-22 11:18:40 -0300

    Corrijo .env para coneccion con el front

--- back_end/eslint.config.mjs
+++ back_end/eslint.config.mjs
@@ -31,7 +31,8 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-unused-vars": "off"
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unsafe-argument": "off"
     },
   },
 );
