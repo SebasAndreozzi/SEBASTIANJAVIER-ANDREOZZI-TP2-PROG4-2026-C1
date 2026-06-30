d97a3eba0951d01ad873b2c3760e67a50f9695cc
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-06-30 08:37:11 -0300

    Corrijo errores de deployment

--- front-end/src/app/app.config.server.ts
+++ front-end/src/app/app.config.server.ts
@@ -1,9 +1,25 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRoutesConfig } from '@angular/ssr';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 import { appConfig } from './app.config';
 
+const serverRoutes: ServerRoute[] = [
+  {
+    path: '/publicaciones/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
+
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [
+    provideServerRendering(),
+    provideServerRoutesConfig(serverRoutes),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { RenderMode } from '@angular/ssr';
 import { Login } from './components/login/login'
 import { Registro } from './components/registro/registro'
 import { Publicaciones } from './components/publicaciones/publicaciones';
@@ -12,7 +11,7 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'signup', component: Registro },
   { path: 'publicaciones', component: Publicaciones, canActivate: [authGuard] },
-  { path: 'publicaciones/:id', component: DetallePublicacion, canActivate: [authGuard], renderMode: RenderMode.Client },
+  { path: 'publicaciones/:id', component: DetallePublicacion, canActivate: [authGuard] },
   { path: 'perfil', component: PerfilUsuario, canActivate: [authGuard] },
   { path: '**', redirectTo: 'login' },
 ];
