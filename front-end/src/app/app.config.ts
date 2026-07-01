2a2bf142175810373926566c6ce1440c87661dba
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2026-07-01 08:12:12 -0300

    Arreglo la implementación de PWA

--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { tokenInterceptor } from './services/token.interceptor';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +14,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:3000',
+    }),
   ],
 };
--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  production: true,
   apiUrl: 'https://sebastianjavier-andreozzi-tp2-prog4-2026.onrender.com',
 };
